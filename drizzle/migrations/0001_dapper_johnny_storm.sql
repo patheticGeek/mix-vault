0bd47c9cfb4bc21e174e570377925739b620b827
Pathetic Geek <geekpathetic@gmail.com>
2026-07-02 10:07:08 +0530

    setup db migrations

---
+++ drizzle/migrations/0001_dapper_johnny_storm.sql
@@ -0,0 +1,3 @@
+CREATE INDEX `idx_tracks_title` ON `tracks` (`title`);--> statement-breakpoint
+CREATE INDEX `idx_tracks_type` ON `tracks` (`type`);--> statement-breakpoint
+CREATE INDEX `idx_tracks_tags` ON `tracks` (`tags`);
