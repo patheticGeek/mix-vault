b5d4c1d86ac31c96bd24f389e44f26011e5d3e8e
Pathetic Geek <geekpathetic@gmail.com>
2026-07-04 13:40:58 +0530

    fix env var build shit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./src/lib/schema.ts",
-  out: "./drizzle",
+  schema: "./src/lib/db/schema.ts",
+  out: "./drizzle/migrations",
   dialect: "sqlite",
   driver: "d1-http",
 } satisfies Config;
